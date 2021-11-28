18982ebc29642fb9e9814c8e1cbe06f6e29fd03a
Thomas Breydo <tbreydo@gmail.com>
2021-11-28 02:37:37 -0500

    Update run_after_post-apply.sh.tmpl Update scripts/install-homebrew.sh

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -7,7 +7,7 @@ if [[ HASROOT -eq 1 ]]; then
 else
     echo Installing Homebrew to ~/.homebrew
     mkdir $HOME/.homebrew
-    pushd
+    pushd .
     cd $HOME/.homebrew
     curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C .homebrew
     eval "$(homebrew/bin/brew shellenv)"
