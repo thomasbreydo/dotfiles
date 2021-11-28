77b67d94ce79f089a9bb02a835ba95338ada3e62
Thomas Breydo <tbreydo@gmail.com>
2021-11-28 02:24:21 -0500

    Add .run_after_post-apply.sh.tmpl.swp Update run_after_post-apply.sh.tmpl Update scripts/install-fish.sh Update scripts/install-homebrew.sh

--- scripts/install-fish.sh
+++ scripts/install-fish.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
+echo Installing fish with brew
 brew install fish
+
+echo Setting the login shell to fish
 which fish >> /etc/shells
 chsh -s `which fish`
 
--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -2,13 +2,13 @@
 
 
 if [[ HASROOT -eq 1 ]]; then
+    echo Installing Homebrew as root
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
+    echo Installing Homebrew to ~/.homebrew
     mkdir $HOME/.homebrew
     curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C .homebrew
     eval "$(homebrew/bin/brew shellenv)"
     brew update --force --quiet
     chmod -R go-w "$(brew --prefix)/share/zsh"
 fi
-
-brew bundle
