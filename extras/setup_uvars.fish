0282e2c5f96e150eff4f4668195a08259ea48e53
Thomas Breydo <tbreydo@gmail.com>
2022-01-22 12:32:58 -0500

    Update extras/setup_uvars.fish Update private_dot_config/iterm2/com.googlecode.iterm2.plist

--- extras/setup_uvars.fish
+++ extras/setup_uvars.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 # vim: set filetype=bash: Vim doesn't handle .fish well
 
-fish_add_path "~/bin/"
+
+# ignoring failure because fish_add_path fails if path exists
+
+fish_add_path -v "~/bin/"; or true
+fish_add_path -v (ruby -e 'print Gem.user_dir'); or true
 
